544dc2e5f9273473d6b299228e6140a7563b7249
Nehal Singh Rajput <52922364+Nehal1s@users.noreply.github.com>
2021-05-29 22:51:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-
-
 // ye jquery vali js hai,.........
-
 var snipp = '<!DOCTYPE html>\n<html lang="en">\n<head>\n<meta charset="UTF-8">\n<meta http-equiv="X-UA-Compatible" content="IE=edge">\n<meta name="viewport" content="width=device-width, initial-scale=1.0">\n<title>Document</title>\n</head>\n<body>\n</body>\n</html>';
 //  var snipp = 'good '//debug
 var prev = ""
@@ -12,7 +9,6 @@ var prev = ""
     }
     blocks($('#code').val())
   });
-
 
 // ye simple es7  js hai,............
   function blocks(c){
@@ -42,7 +38,4 @@ var prev = ""
         lineno+=1
       }
     }
-
   }
-  
-  
